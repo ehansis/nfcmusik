06655ee267f0aa80191f0fe97cf1a88965194156
Carsten Rösnick-Neugebauer <croesnick@gmail.com>
2019-10-22 23:36:59 +0200

    Update packer config to use ansible with chroot connection
    Also: improve vagrant box setup script

--- setup/packer/setup.sh
+++ setup/packer/setup.sh
@@ -2,8 +2,11 @@
 
 set -eux
 
-GOROOT=/usr/lib/go
+PACKERFILE=/nfcmusik/setup/packer/packer.json
+
+GOROOT=/usr/lib/go-1.12
 GOPATH=$HOME/work
+
 PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 function install_packer() {
@@ -11,59 +14,52 @@ function install_packer() {
 
   curl --remote-name --silent --show-error "https://releases.hashicorp.com/packer/${version}/packer_${version}_linux_amd64.zip"
   unzip "packer_${version}_linux_amd64.zip"
-  sudo mv packer /usr/local/bin
   rm -f "packer_${version}_linux_amd64.zip"
+  sudo mv packer /usr/local/bin
 }
 
 function build_packer_arm_builder_plugin() {
-  mkdir -p "${GOPATH}/src/github.com/solo-io/"
-  pushd "${GOPATH}/src/github.com/solo-io/"
+  local src_dir="${GOPATH}/src/github.com/solo-io/packer-builder-arm-image"
+  mkdir -p "$(dirname "${src_dir}")"
 
-  git clone https://github.com/solo-io/packer-builder-arm-image.git packer-builder-arm-image
-  pushd ./packer-builder-arm-image
-  go get -d -v
+  git clone https://github.com/solo-io/packer-builder-arm-image.git "${src_dir}"
+  pushd "${src_dir}"
+
+  # NOTE Temporary fix; can be removed one the branch's accompanying PR is reviewd and merged
+  git remote add fork https://github.com/croesnick/packer-builder-arm-image.git
+  git pull fork feature/param-mount_path
+
   go build
+  popd
 
-  popd
-  popd
+  sudo mv "${src_dir}/packer-builder-arm-image" /usr/local/bin/
 }
 
-# Update the system
+function build_rpi_image() {
+  PACKER_LOG_FILE=$(mktemp)
+  sudo PACKER_LOG=1 packer build "${PACKERFILE}" | tee "${PACKER_LOG_FILE}"
+
+  BUILD_NAME=$(grep -Po "(?<=Build ').*(?=' finished.)" "${PACKER_LOG_FILE}")
+  IMAGE_PATH=$(grep -Po "(?<=--> ${BUILD_NAME}: ).*" "${PACKER_LOG_FILE}")
+
+  sudo mv "/home/vagrant/${IMAGE_PATH}" "/nfcmusik/build/nfcmusik-rpi-arm.img"
+}
+
 sudo apt-get update -qq
-sudo apt-get install -y software-properties-common
-
-# Add the golang repo
-sudo add-apt-repository --yes ppa:gophers/archive
-
-# Install required packages
-sudo apt-get update
-sudo apt-get install -y \
+sudo apt-get -y install \
   curl \
   gcc \
   git \
-  golang-go \
+  golang-1.12-go \
   kpartx \
+  python3 \
+  python3-pip \
   qemu-user-static \
   unzip
 
+sudo pip3 install --system ansible
 go get -u github.com/golang/dep/cmd/dep
-install_packer '1.4.4'
-packer_arm_builder_plugin_dir="$( build_packer_arm_builder_plugin )"
 
-mkdir -p /home/vagrant/.packer.d/plugins
-cp "${packer_arm_builder_plugin_dir}/packer-builder-arm-image/packer-builder-arm-image" /home/vagrant/.packer.d/plugins/
-
-PACKER_LOG=$(mktemp)
-packer build "${PACKERFILE}" | tee "${PACKER_LOG}"
-
-BUILD_NAME=$(grep -Po "(?<=Build ').*(?=' finished.)" ${PACKER_LOG})
-IMAGE_PATH=$(grep -Po "(?<=--> ${BUILD_NAME}: ).*" ${PACKER_LOG})
-rm -f "${PACKER_LOG}"
-
-# If the new image is there, copy it out or throw an error
-if [[ -f /home/vagrant/${IMAGE_PATH} ]]; then
-  sudo cp "/home/vagrant/${IMAGE_PATH}" "/nfcmusik/${IMAGE_PATH%/image}.img"
-else
-  echo "Error: Unable to find build artifact."
-  exit
-fi
+install_packer '1.3.5'
+build_packer_arm_builder_plugin
+build_rpi_image
