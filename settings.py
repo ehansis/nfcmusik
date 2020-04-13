d8973f434bdc72519bc4d14674ea7ca97f9b6063
Eberhard Hansis <git@ehansis.de>
2020-04-13 13:25:09 +0200

    Fix default path for music folder

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ import os
 
 SERVER_HOST_MASK = os.environ.get('NFCMUSIK_SERVER_HOST', default='0.0.0.0')
 SERVER_PORT = os.environ.get('NFCMUSIK_SERVER_PORT', 5000)
-MUSIC_ROOT = os.environ.get('NFCMUSIK_AUDIO_FILE_ROOT', '~/music')
+MUSIC_ROOT = os.environ.get('NFCMUSIK_AUDIO_FILE_ROOT', '/usr/local/nfcmusik/music')
 DEFAULT_VOLUME = os.environ.get('NFCMUSIK_AUDIO_VOLUME', 70)
